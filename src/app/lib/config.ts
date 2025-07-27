4c08b4b5c987b5ddc30813d674ee5e071b62e68a
pietro <pietro.peerani@gmail.com>
2025-07-27 15:02:40 +0200

    feat(pages): add /links page

--- src/app/lib/config.ts
+++ src/app/lib/config.ts
@@ -1,50 +1,76 @@
 import { SiteConfig } from "./types/config";
+import { LinksItems } from "./types/links";
 import { NavItem, SocialLink } from "./types/nav";
 
 export const siteConfig: SiteConfig = {
-    version: '0.0.1',
-    title: 'Basilite',
-    description:
-        '',
-    github: 'https://github.com/basilite/www',
-    npm: '',
-    quickLinks: [
-        { title: 'Customize', href: '/docs/customize' },
-        { title: 'Examples', href: '/docs/examples' }
-    ],
-    logo: '/logo.svg',
-    logoDark: '/logo-white.svg',
-    favicon: '/favicon.png',
+  version: "0.0.1",
+  title: "Basilite",
+  description: "",
+  github: "https://github.com/basilite/www",
+  npm: "",
+  quickLinks: [
+    { title: "Customize", href: "/docs/customize" },
+    { title: "Examples", href: "/docs/examples" },
+  ],
+  logo: "/logo.svg",
+  logoDark: "/logo-white.svg",
+  favicon: "/favicon.png",
 };
 
-
 export let navItems: NavItem[] = [
-    {
-        title: 'Products',
-        href: '/products'
-    },
-    {
-        title: "Docs",
-        href: "https://docs.basilite.org"
-    },
-    {
-        title: "Feature",
-        href: "https://feature.basilite.org"
-    },
-    {
-        title: "Vision",
-        href: "/vision"
-    },
-    {
-        title: 'About',
-        href: '/about'
-    }
+  {
+    title: "Products",
+    href: "/products",
+  },
+  {
+    title: "Docs",
+    href: "https://docs.basilite.org",
+  },
+  {
+    title: "Feature",
+    href: "https://feature.basilite.org",
+  },
+  {
+    title: "Vision",
+    href: "/vision",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
 ];
 
-export let socialLinks: SocialLink[] = [
-    {
-        title: 'GitHub',
-        href: 'https://github.com/basilite',
-        icon: 'github'
+export const socialLinks: SocialLink[] = [
+  {
+    title: "GitHub",
+    href: "https://github.com/basilite",
+    icon: "github",
+  },
+];
+
+export const linksItems: LinksItems[] = [
+  {
+    image: "/links/feature-request.png",
+    link: "https://basilite.featurebase.app/",
+    size: {
+      rows: 1,
+      cols: 2,
     },
+  },
+  {
+    image: "/links/github-background.png",
+    link: "https://github.com/basilite",
+    size: {
+      rows: 2,
+      cols: 2,
+    },
+  },
+  {
+    video:
+      "/links/showreel.mp4",
+    size: {
+      rows: 1,
+      cols: 2,
+    },
+  },
 ];
---
+++ src/app/lib/types/links.ts
@@ -0,0 +1,10 @@
+export interface LinksItems {
+  image?: string;
+  video?: string;
+  link?: string;
+  size: {
+    rows: number;
+    cols: number;
+  };
+  label?: string;
+}
